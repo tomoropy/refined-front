d312d93d902ddf2782e6f3ed1ea1cd9aa6ecc42e
tomoropy <ucchi1910@gmail.com>
2023-03-16 16:56:57 +0900

    [fix]デザイン修正

--- src/components/UserLists.tsx
+++ src/components/UserLists.tsx
@@ -15,16 +15,22 @@ const UsersList: NextPage<UsersListProps> = () => {
 
   return (
     <>
-      {users.map((user: User) => (
-        <div>
-          <p>username: {user.username}</p>
-          <p>email: {user.email}</p>
-          <p>password: {user.password}</p>
-          <p>text: {user.text}</p>
-        </div>
-      ))}
+        <div className="flex flex-wrap justify-center items-start">
+        {users.map((user: User) => (
+          <div key={user.uid} className="bg-slate-200 rounded-md border-2 border-gray-300 m-4 p-4">
+              <p><span className='font-bold'>Username:</span> {user.username}</p>
+              <p><span className='font-bold'>Email:</span> {user.email}</p>
+              <p><span className='font-bold'>Password:</span> {user.password}</p>
+              <p><span className='font-bold'>Text:</span> {user.text}</p>
+              <p><span className='font-bold'>Avatar URL:</span> {user.avater}</p>
+              <p><span className='font-bold'>Header URL:</span> {user.header}</p>
+              <p><span className='font-bold'>Created At:</span> {user.createdAt}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
 
 export default UsersList;
+
